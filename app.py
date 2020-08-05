ed578d4feada7d6a7727427b2f038aae7af112bf
Krishnavamsi <vamsik532@gmail.com>
2020-08-05 17:06:48 +0530

    removed commented code

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ import random
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        # input_df = None
-        # if request.form:
-        #     input_df = pd.DataFrame(request.form.to_dict(flat=False))
-        #     input_df.drop(columns = ['submit'], inplace = True)
-        # if not input_df.empty:
-        #     ans = predict(input_df)
-        #     type_ = PersonalityTypes.query.filter_by(ptype = ans).first()
         pass
     else:
         facts = Facts.query.all()
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from __init__ import app, db
 from flask_sqlalchemy import SQLAlchemy
 
+#stores information related to four major personality E-S-N-P-I
 class FourDichotomy(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
@@ -11,6 +12,7 @@ class FourDichotomy(db.Model):
     def __repr__(self):
         return '<Personality Type %r>' % self.type
 
+#store information related 16 personality types
 class PersonalityTypes(db.Model):
 
     ptype = db.Column(db.String(200), primary_key = True)
@@ -22,6 +24,7 @@ class PersonalityTypes(db.Model):
     def __repr__(self):
         return '<Personality Name %r>' % self.ptype
 
+#to store information related to personality facts
 class Facts(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
